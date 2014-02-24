5beb34e98915d216fabfc8f55f11c3be597a5183
unknown <yuspan@PBB-C320R-13.iowa.uiowa.edu>
2014-02-24 11:16:21 -0600

    2nd commit

---
+++ Web/default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="default.aspx.vb" Inherits="_default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <title>Salary Calculator</title>
+</head>
+<body>
+    <h1>Salary Calculator</h1>
+&nbsp;<form id="form1" runat="server">
+    <div>
+    Wages&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="WagesText" runat="server"></asp:TextBox>
+        <br />
+        No. of Hours&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="HoursText" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        Deduction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="deductiontext" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Submit" runat="server" Text="Submit" />
+        <br />
+        Money To The Damn Government
+        <asp:Label ID="TaxPaid" runat="server"></asp:Label>
+        <br />
+        Net Pay&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Netpaylbl" runat="server"></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Web/default.aspx.vb
@@ -0,0 +1,49 @@
+﻿
+Partial Class _default
+
+    Inherits System.Web.UI.Page
+
+    Protected Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+
+        'Specify constant values
+        Const tax_rate1 As Decimal = 0.18
+        Const tax_rate2 As Decimal = 0.22
+
+        'Create bariables to hold the values entered by the user
+        Dim w As Decimal = WagesText.Text
+        Dim h As Decimal = HoursText.Text
+        Dim d As Decimal = deductiontext.Text
+        Const Hour_limit As Decimal = 40
+
+        Dim GrossEarning As Decimal
+        If h <= Hour_limit Then
+            GrossEarning = h * w
+        Else
+            GrossEarning = (h - Hour_limit) * (1.5 * w) + Hour_limit * w
+
+        End If
+
+
+        Dim TaxableEarning As Decimal
+        TaxableEarning = GrossEarning - d
+
+        Dim Tax As Decimal
+
+        If GrossEarning <= 500 Then
+            Tax = tax_rate1 * TaxableEarning
+        Else
+            Tax = tax_rate2 * TaxableEarning
+
+
+        End If
+
+        Dim NetEarnings As Decimal
+        NetEarnings = TaxableEarning - Tax
+
+        'Display
+        TaxPaid.Text = "$" & Tax.ToString("#,####.##")
+        Netpaylbl.Text = "You are paid by $" & NetEarnings.ToString("#,####.#")
+
+
+    End Sub
+End Class
